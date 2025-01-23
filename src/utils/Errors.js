50877528b98f7063e82dc0050e054438ffc95754
Gabriel Vieira <nox030705@gmail.com>
2025-01-23 11:32:38 -0400

    chore: created 'Errors' class

--- src/utils/Errors.js
+++ src/utils/Errors.js
@@ -1,11 +1,12 @@
-import { ARGUMENT_ERRORS } from "./const/Errors.js";
 import Info from "./Info.js";
 import chalk from "chalk";
 
-export function argumentError(args, type) {
-    if (type == ARGUMENT_ERRORS.unexpected) {
-        console.error(`${chalk.bold.red("ERROR:")} unexpected argument[s] --> ${chalk.bold.red(args.slice(1).join(","))}, only 1 argument expected`);
-    } else if (type == ARGUMENT_ERRORS.invalidFlag) {
+export default class Errors {
+    static unexpectedArgument(args) {
+        console.error(`${chalk.bold.red("ERROR:")} unexpected argument[s] --> ${chalk.bold.red(args.slice(1).join(","))}, only 1 argument expected\n${Info.flags()}\n${Info.subcommands()}`);
+    }
+    static invalidFlag(args) {
         console.error(`${chalk.bold.red("ERROR:")} invalid flag -> ${chalk.bold.red(args[0])}\n${Info.flags()}`)
     }
 }
+
