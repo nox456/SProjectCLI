0816f040179c277fd6db34552bd096682d952b9d
Gabriel Vieira <nox030705@gmail.com>
2025-01-27 10:30:04 -0400

    feat(flags): show help when run app without arguments

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import Args from "../utils/Args.js";
 
+import help from "../flags/help.js";
+
 const args = process.argv.slice(2)
 
-if (!Args.validate(args)) {
+if (args.length == 0) {
+    help.handler()
+    process.exit(0)
+} else if (!Args.validate(args)) {
     process.exit(1)
 }
 
@@ -12,4 +17,5 @@ if (args[0].startsWith("-")) {
     process.exit(0)
 } else {
     await Args.handleSubcommand(args[0])
+    process.exit(0)
 }
